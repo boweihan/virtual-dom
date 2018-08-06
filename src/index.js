6bf4c9126e76b7735c9ecc261e03cd8541ae5722
bowei <bowei.han100@gmail.com>
2018-08-05 23:50:45 -0400

    basic diffing engine

--- src/index.js
+++ src/index.js
@@ -2,15 +2,22 @@
 // React.createElement() or h()
 /** @jsx h */
 
-// Custom implementation of h()
-function h(type, props, ...children) {
+// custom implementation of h()
+const h = (type, props, ...children) => {
   return { type, props, children };
-}
+};
 
-// Virtual DOM implementation
+// virtual DOM implementation
 
-// Creates the root node
-function createElement(node) {
+/**
+ * create a new node
+ * @param node virtual dom node representation
+ */
+const createElement = node => {
+  if (!node) {
+    // handle falsy nodes
+    return;
+  }
   if (typeof node === 'string') {
     return document.createTextNode(node);
   }
@@ -20,7 +27,45 @@ function createElement(node) {
     el.appendChild.bind(el),
   );
   return el;
-}
+};
+
+/**
+ * handle diff for a node on the virtual dom
+ * @param parent parent DOM element
+ * @param newNode new child virtual dom node
+ * @param oldNode old child virtual dom node
+ * @param index index of child element, for O(1) access
+ *
+ */
+const updateElement = (
+  parent,
+  newNode,
+  oldNode,
+  index = 0 /* index of child */,
+) => {
+  if (newNode && !oldNode) {
+    // there is a new node and no old node, create the new node
+    parent.appendChild(createElement(newNode));
+  } else if (!newNode && oldNode) {
+    // there is no new node but there was an old node, remove the old node from the DOM
+    parent.removeChild(parent.childNodes[index]);
+  } else if (changed(newNode, oldNode)) {
+    parent.replaceChild(createElement(newNode), parent.childNodes[index]);
+  }
+};
+
+/**
+ * determine whether nodes should be diffed
+ * @param node1 virtual dom node
+ * @param node2 virtual dom node
+ */
+const changed = (node1, node2) => {
+  return (
+    typeof node1 !== typeof node2 ||
+    (typeof node1 === 'string' && node1 !== node2) ||
+    node1.type !== node2.type
+  );
+};
 
 // sample DOM tree
 const a = (
@@ -31,3 +76,5 @@ const a = (
 );
 
 console.log(a);
+let el = createElement(a);
+console.log(el);
