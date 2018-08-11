6e8481c29b2f3e4986f44abcd14e2896ededd2ca
bowei <bowei.han100@gmail.com>
2018-08-10 21:28:52 -0400

    add extends jsdocs

--- src/vdom/BroadcastingVirtualDom.js
+++ src/vdom/BroadcastingVirtualDom.js
@@ -4,10 +4,12 @@ import VirtualDom from './VirtualDom';
 import type { Node, Element, Props, Value } from '../types/types';
 
 export default class BroadcastingVirtualDom extends VirtualDom {
+  /** @extends **/
   createElement(node?: Node): ?Element | ?Text {
     return super.createElement(node);
   }
 
+  /** @extends **/
   updateElement(
     $parent: Element,
     newNode: Node,
@@ -17,18 +19,22 @@ export default class BroadcastingVirtualDom extends VirtualDom {
     super.updateElement($parent, newNode, oldNode, index);
   }
 
+  /** @extends **/
   setProp($target: Element, name: string, value: Value): void {
     super.setProp($target, name, value);
   }
 
+  /** @extends **/
   setProps($target: Element, props: Props): void {
     super.setProps($target, props);
   }
 
+  /** @extends **/
   removeProp($target: Element, name: string, value: Value): void {
     super.removeProp($target, name, value);
   }
 
+  /** @extends **/
   updateProp(
     $target: Element,
     name: string,
@@ -38,10 +44,12 @@ export default class BroadcastingVirtualDom extends VirtualDom {
     super.updateProp($target, name, newVal, oldVal);
   }
 
+  /** @extends **/
   updateProps($target: Element, newProps: Props, oldProps: Props = {}): void {
     super.updateProps($target, newProps, oldProps);
   }
 
+  /** @extends **/
   addEventListeners($target: Element, props: Props): void {
     super.addEventListeners($target, props);
   }
